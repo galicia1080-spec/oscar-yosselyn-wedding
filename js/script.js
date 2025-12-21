abb78a2fcf8ef437495091f4e33e63f7d47fc1dd
Oscar Galicia <galicia1080@gmail.com>
2025-12-20 23:09:20 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const envelope = document.getElementById("envelope");
 const cover = document.getElementById("cover");
 const story = document.getElementById("story");
+const music = document.getElementById("music");
 
 envelope.addEventListener("click", () => {
   cover.style.display = "none";
   story.style.display = "block";
+
+  music.volume = 0.4;
+  music.play();
 });
